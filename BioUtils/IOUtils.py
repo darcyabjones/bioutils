5cb8d8722f6eb4caab127610d69695910df98a75
Darcy Adam Bain Jones <darcyabjones@localhost.localdomain>
2014-08-05 16:37:21 +1000

    added

---
+++ BioUtils/IOUtils.py
@@ -0,0 +1,30 @@
+
+__program__ = "IOUtils"
+__version__ = "0.1.0"
+__author__ = "Darcy Jones"
+__date__ = "17 May 2014"
+__author_email__ = "darcy.ab.jones@gmail.com"
+__license__ = """
+##############################################################################
+
+    Copyright (C) 2014  Darcy Jones
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+##############################################################################
+"""
+
+############################ Import all modules ##############################
+
+import os
